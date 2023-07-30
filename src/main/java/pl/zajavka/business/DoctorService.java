9176e32013f308d57b4a8e4e494a9ff09b08d2ee
Maciej Wiercinski <wiercinskimaciejj@gmail.com>
2023-07-30 18:46:56 +0200

    add dao class

---
+++ src/main/java/pl/zajavka/business/DoctorService.java
@@ -0,0 +1,38 @@
+package pl.zajavka.business;
+
+import jakarta.transaction.Transactional;
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.data.crossstore.ChangeSetPersister;
+import org.springframework.stereotype.Service;
+import pl.zajavka.business.dao.DoctorDAO;
+import pl.zajavka.domain.Doctor;
+import pl.zajavka.domain.exception.NotFoundException;
+
+import java.util.List;
+import java.util.Optional;
+
+@Slf4j
+@Service
+@AllArgsConstructor
+public class DoctorService {
+
+    private final DoctorDAO doctorDAO;
+
+    @Transactional
+    public List<Doctor> findAvailable() {
+        List<Doctor> availableDoctors = doctorDAO.findAvailable();
+        log.info("Available Doctor: [{}]", availableDoctors.size());
+        return availableDoctors;
+    }
+
+    @Transactional
+    public Doctor findDoctor(String pesel) {
+        Optional<Doctor> doctor = doctorDAO.findByPesel(pesel);
+        if (doctor.isEmpty()) {
+            throw new NotFoundException("Could not find doctor by pesel: [%s]".formatted(pesel));
+        }
+        return doctor.get();
+    }
+}
---
+++ src/main/java/pl/zajavka/business/dao/DoctorDAO.java
@@ -0,0 +1,13 @@
+package pl.zajavka.business.dao;
+
+import pl.zajavka.domain.Doctor;
+
+import java.util.List;
+import java.util.Optional;
+
+public interface DoctorDAO {
+
+    List<Doctor> findAvailable();
+
+    Optional<Doctor> findByPesel(String pesel);
+}
